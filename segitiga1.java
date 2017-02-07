d9538a8c11ae054054e3ee0d639e56066828d6b7
BobbyGustafyan <mrbobbygustafyan@gmail.com>
2017-02-07 11:11:58 +0700

    Add files via upload

---
+++ segitiga1.java
@@ -0,0 +1,13 @@
+public class segitiga1 {
+public static void main (String[]args) {
+System.out.println("PIRAMIDA SETENGAH KANAN");
+for (int a=1; a<=5; a++) {
+for (int b=1; b<=a-1; b++) {
+System.out.print("*");
+}
+System.out.println("*");
+}
+System.out.println("\n");
+}
+} 
+
